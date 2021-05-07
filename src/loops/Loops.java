e8b257a11395f01e4f6c5343a17ebb2536faa8de
Arthur Cardoso de Jesus <arthurcardosoj@gmail.com>
2021-05-07 19:05:19 -0300

    Uma atividade feita no dia 07/05/2021 TEste basico de um loop

---
+++ src/loops/Loops.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loops;
+
+/**
+ *
+ * @author baung
+ */
+public class Loops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        int cont;
+        cont = 1;
+        
+        while(cont <= 100 ){
+        if (cont % 3 == 0 || cont % 5 ==0){System.out.println(cont);}
+        cont++;
+       
+            }
+        System.out.println("Acabou meu guri!");
+    }
+    
+}
